aedc6656ea144e631cfd8b39a87d1cfd763873a5
yoshixmk <pirori67@gmail.com>
2020-07-11 23:25:00 +0900

    std versioning for eggs updating and update pagic

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export { GithubDatabaseEntry } from "./github.ts";
-export { encode } from "https://deno.land/std/encoding/base64.ts";
+export { encode } from "https://deno.land/std@0.60.0/encoding/base64.ts";
 export {
   Markdown,
   link,

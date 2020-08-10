948fa6344b7f73440965229cdd1cebeffea0bac2
yoshixmk <yoshixmk@users.noreply.github.com>
2020-08-10 06:20:15 +0000

    deploy: 5ca836f9d6dcee50fed303fa05c71f325842c528

--- _head.js
+++ _head.js
@@ -5,7 +5,7 @@ const Head = ({ config, title, ga, outputPath, isDark }) => {
     const shouldSetIsDark = document.cookie.includes('is_dark=1') ? true : document.cookie.includes('is_dark=0') ? false : window.matchMedia('(prefers-color-scheme: dark)').matches;
     if (shouldSetIsDark) {
       document.documentElement.classList.add('is_dark');
-      document.getElementById('prismTheme').href = "${config.base}assets/prism_tomorrow.css";
+      document.getElementById('prismTheme').href = "${config.root}assets/prism_tomorrow.css";
     }
   `;
     return (React.createElement("head", null,
@@ -14,11 +14,11 @@ const Head = ({ config, title, ga, outputPath, isDark }) => {
         React.createElement(Helmet, null,
             React.createElement("meta", { charSet: "utf-8" }),
             React.createElement("meta", { httpEquiv: "x-ua-compatible", content: "ie=edge" }),
-            React.createElement("title", null, outputPath !== 'index.html' ? `${title} · ${config.title}` : title),
+            React.createElement("title", null, title ? (outputPath !== 'index.html' ? `${title} · ${config.title}` : title) : config.title),
             config.description && React.createElement("meta", { name: "description", content: config.description }),
             React.createElement("meta", { name: "viewport", content: "width=device-width, initial-scale=1" }),
-            React.createElement("link", { rel: "stylesheet", href: `${config.base}assets/index.css` }),
-            React.createElement("link", { id: "prismTheme", rel: "stylesheet", href: isDark ? `${config.base}assets/prism_tomorrow.css` : `${config.base}assets/prism.css` }),
+            React.createElement("link", { id: "prismTheme", rel: "stylesheet", href: isDark ? `${config.root}assets/prism_tomorrow.css` : `${config.root}assets/prism.css` }),
+            React.createElement("link", { rel: "stylesheet", href: `${config.root}assets/index.css` }),
             React.createElement("script", null, scriptSetIsDark))));
 };
 export default Head;

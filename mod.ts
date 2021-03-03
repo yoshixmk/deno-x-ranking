cb0685483bee757b6f39f08d88cbeb3a4ff11487
yoshixmk <pirori67@gmail.com>
2021-03-03 23:19:12 +0900

    wip: deleted

--- mod.ts
+++ mod.ts
@@ -1,60 +1,60 @@
-import { green, PARSE_FAILURE, red } from "./deps.ts";
+import { green, red } from "./deps.ts";
 import { consoleTable } from "./src/console_table.ts";
 import { Csv, MarkdownFile, Table, Tsv } from "./src/domains/Format.ts";
 import { SeparatedFileCreator } from "./src/file_creator.ts";
 import { generateMarkdownFile } from "./src/markdown_file_creator.ts";
-import { parseArgs } from "./src/services/arg_service.ts";
+import { parsedValue } from "./src/services/arg_service.ts";
 import { githubService } from "./src/services/github_service.ts";
 import { registryService } from "./src/services/registry_service.ts";
+import { parse } from "./deps.ts";
 
-const parser = parseArgs();
+const res = parsedValue(parse(Deno.args));
+Deno.exit(0);
 
-const res = parser.parse(Deno.args);
+// if (res.tag === PARSE_FAILURE) {
+//   console.error("Failed to parse CLI arguments");
+//   console.error(res.error.toString());
+//   Deno.exit(1);
+// }
+// console.dir(res.value);
+// const { help, username, token, format, outputFile, sampling } = res.value;
 
-if (res.tag === PARSE_FAILURE) {
-  console.error("Failed to parse CLI arguments");
-  console.error(res.error.toString());
-  Deno.exit(1);
-}
-console.dir(res.value);
-const { help, username, token, format, outputFile, sampling } = res.value;
+// if (username === undefined || token === undefined) {
+//   console.log(red("Needs to input both username and token."));
+//   Deno.exit(1);
+// }
+// if (format === undefined) {
+//   console.log(red("Needs to input output format."));
+//   Deno.exit(1);
+// }
 
-if (username === undefined || token === undefined) {
-  console.log(red("Needs to input both username and token."));
-  Deno.exit(1);
-}
-if (format === undefined) {
-  console.log(red("Needs to input output format."));
-  Deno.exit(1);
-}
+// console.debug(green(`Started. format = ${format}`));
 
-console.debug(green(`Started. format = ${format}`));
+// const isSampling = sampling !== "false";
+// const githubEntries = await registryService.getGithubEntries(isSampling);
 
-const isSampling = sampling !== "false";
-const githubEntries = await registryService.getGithubEntries(isSampling);
+// const githubRepositories = githubService.getGithubRepositories(
+//   githubEntries,
+//   username,
+//   token,
+// );
 
-const githubRepositories = githubService.getGithubRepositories(
-  githubEntries,
-  username,
-  token,
-);
+// const rankingEntries = await githubRepositories;
+// switch (format) {
+//   case Table:
+//     consoleTable(rankingEntries);
+//     break;
+//   case Tsv:
+//   case Csv:
+//     await new SeparatedFileCreator(format, outputFile).generateFile(
+//       rankingEntries,
+//     );
+//     break;
+//   case MarkdownFile:
+//     await generateMarkdownFile(rankingEntries);
+//     break;
+//   default:
+//     throw Error(`Cannot use the format, ${format}`);
+// }
 
-const rankingEntries = await githubRepositories;
-switch (format) {
-  case Table:
-    consoleTable(rankingEntries);
-    break;
-  case Tsv:
-  case Csv:
-    await new SeparatedFileCreator(format, outputFile).generateFile(
-      rankingEntries,
-    );
-    break;
-  case MarkdownFile:
-    await generateMarkdownFile(rankingEntries);
-    break;
-  default:
-    throw Error(`Cannot use the format, ${format}`);
-}
-
-console.debug(green(`End. format = ${format}`));
+// console.debug(green(`End. format = ${format}`));
--- src/services/arg_service.ts
+++ src/services/arg_service.ts
@@ -1,77 +1,63 @@
-import {
-  args,
-  Choice,
-  EarlyExitFlag,
-  Option,
-  PartialOption,
-} from "../../deps.ts";
-import { Text } from "../../deps.ts";
+import { Args } from "../../deps.ts";
 import { Csv, Format, MarkdownFile, Table, Tsv } from "../domains/Format.ts";
 
-export const parseArgs = () => {
-  const parser = args
-    .describe("Add or subtract two numbers")
-    .with(
-      EarlyExitFlag("help", {
-        describe: "Show help",
-        alias: ["h"],
-        exit() {
-          console.log(parser.help());
-          return Deno.exit();
-        },
-      }),
-    )
-    .with(
-      Option("username", {
-        type: Text,
-        describe: "Github account username without '@'. required token",
-        alias: ["u"],
-      }),
-    )
-    .with(
-      Option("token", {
-        type: Text,
-        describe: "Github account token. required username",
-        alias: ["t"],
-      }),
-    )
-    .with(
-      Option("format", {
-        type: Choice<Format>(
-          {
-            value: Tsv,
-            describe: "Output tsv file",
-          },
-          {
-            value: Csv,
-            describe: "Output csv file",
-          },
-          {
-            value: Table,
-            describe: "Output console table",
-          },
-          {
-            value: MarkdownFile,
-            describe: "Output markdown file",
-          },
-        ),
-        alias: ["f"],
-        describe: "Choice output format",
-      }),
-    ).with(
-      PartialOption("outputFile", {
-        type: Text,
-        describe: "Output file path. Only file like tsv and csv.",
-        alias: ["o"],
-        default: "",
-      }),
-    ).with(
-      PartialOption("sampling", {
-        type: Text,
-        describe:
-          "For testing, fetch a little sample from API. --sampling true",
-        default: "false",
-      }),
-    );
-  return parser;
+class Flag {
+  private name: string;
+  private alias?: string;
+  private describe?: string;
+  private availableValues: any[];
+  constructor({name, alias, describe, availableValues}: {name: string, alias?: string, describe?: string, availableValues?: any[]}) {
+    this.name = name;
+    this.alias = alias;
+    this.describe = describe;
+    this.availableValues = availableValues ?? [];
+  }
+}
+
+const help = new Flag({name: "help", alias: "h", describe: "Show help"});
+const username = new Flag({name: "username", alias: "u", describe: "Github account username without '@'. required token"});
+const token = new Flag({name: "token", alias: "t", describe: "Github account token. required username"});
+const format = new Flag({name: "format", alias: "f", describe: "Output format. tsv file, csv file, markdown file, or console table"});
+
+export const parsedValue = (args: Args) => {
+  console.dir(args)
+  //   .with(
+  //     Option("format", {
+  //       type: Choice<Format>(
+  //         {
+  //           value: Tsv,
+  //           describe: "Output tsv file",
+  //         },
+  //         {
+  //           value: Csv,
+  //           describe: "Output csv file",
+  //         },
+  //         {
+  //           value: Table,
+  //           describe: "Output console table",
+  //         },
+  //         {
+  //           value: MarkdownFile,
+  //           describe: "Output markdown file",
+  //         },
+  //       ),
+  //       alias: ["f"],
+  //       describe: "Choice output format",
+  //     }),
+  //   ).with(
+  //     PartialOption("outputFile", {
+  //       type: Text,
+  //       describe: "Output file path. Only file like tsv and csv.",
+  //       alias: ["o"],
+  //       default: "",
+  //     }),
+  //   ).with(
+  //     PartialOption("sampling", {
+  //       type: Text,
+  //       describe:
+  //         "For testing, fetch a little sample from API. --sampling true",
+  //       default: "false",
+  //     }),
+  //   );
+  // return parser;
 };

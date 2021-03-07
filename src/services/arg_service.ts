0776b5d122715573977489e0f5ceb1d7e487fb04
yoshixmk <pirori67@gmail.com>
2021-03-07 21:15:09 +0900

    create Help

--- src/services/arg_service.ts
+++ src/services/arg_service.ts
@@ -6,58 +6,86 @@ class Flag {
   private alias?: string;
   private describe?: string;
   private availableValues: any[];
-  constructor({name, alias, describe, availableValues}: {name: string, alias?: string, describe?: string, availableValues?: any[]}) {
+  constructor(
+    { name, alias, describe, availableValues }: {
+      name: string;
+      alias?: string;
+      describe?: string;
+      availableValues?: any[];
+    },
+  ) {
     this.name = name;
     this.alias = alias;
     this.describe = describe;
     this.availableValues = availableValues ?? [];
   }
+  get getName() {
+    return this.name;
+  }
+  get getAlias() {
+    return this.alias;
+  }
+  get getDescribe() {
+    return this.describe;
+  }
+  get getAvailableValues() {
+    return this.availableValues;
+  }
+  exists(args: Args): Boolean {
+    return args[this.alias ?? this.name] !== undefined ||
+      args[this.name] !== undefined;
+  }
+  toString(): String {
+    return `--${this.name} (-${this.alias ?? ""}):\t${this.describe}`;
+  }
 }
 
-const help = new Flag({name: "help", alias: "h", describe: "Show help"});
-const username = new Flag({name: "username", alias: "u", describe: "Github account username without '@'. required token"});
-const token = new Flag({name: "token", alias: "t", describe: "Github account token. required username"});
-const format = new Flag({name: "format", alias: "f", describe: "Output format. tsv file, csv file, markdown file, or console table"});
+const help = new Flag({ name: "help", alias: "h", describe: "Show help" });
+const username = new Flag({
+  name: "username",
+  alias: "u",
+  describe: "Github account username without '@'. required token",
+});
+const token = new Flag({
+  name: "token",
+  alias: "t",
+  describe: "Github account token. required username",
+});
+const format = new Flag({
+  name: "format",
+  alias: "f",
+  describe:
+    "Output format. tsv file, csv file, markdown file, or console table",
+  availableValues: [
+    Tsv,
+    Csv,
+    Table,
+    MarkdownFile,
+  ],
+});
+const outputFile = new Flag({
+  name: "outputFile",
+  alias: "o",
+  describe: "Output file path. Only file like tsv and csv.",
+});
+const sampling = new Flag({
+  name: "sampling",
+  alias: "s",
+  describe: "For testing, fetch a little sample from API. --sampling true",
+});
+const allFlags: Array<Flag> = [
+  help,
+  username,
+  token,
+  format,
+  outputFile,
+  sampling,
+];
 
 export const parsedValue = (args: Args) => {
-  console.dir(args)
-  //   .with(
-  //     Option("format", {
-  //       type: Choice<Format>(
-  //         {
-  //           value: Tsv,
-  //           describe: "Output tsv file",
-  //         },
-  //         {
-  //           value: Csv,
-  //           describe: "Output csv file",
-  //         },
-  //         {
-  //           value: Table,
-  //           describe: "Output console table",
-  //         },
-  //         {
-  //           value: MarkdownFile,
-  //           describe: "Output markdown file",
-  //         },
-  //       ),
-  //       alias: ["f"],
-  //       describe: "Choice output format",
-  //     }),
-  //   ).with(
-  //     PartialOption("outputFile", {
-  //       type: Text,
-  //       describe: "Output file path. Only file like tsv and csv.",
-  //       alias: ["o"],
-  //       default: "",
-  //     }),
-  //   ).with(
-  //     PartialOption("sampling", {
-  //       type: Text,
-  //       describe:
-  //         "For testing, fetch a little sample from API. --sampling true",
-  //       default: "false",
-  //     }),
-  //   );
-  // return parser;
+  console.dir(args);
+
+  if (help.exists(args)) {
+    allFlags.forEach((flag: Flag) => console.log(flag.toString()));
+  }
 };

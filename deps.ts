cbe96fe8195aeb987c22d6e6516bdd2e1e62602c
yoshixmk <pirori67@gmail.com>
2021-08-16 21:44:14 +0900

    updated deps versions

--- deps.ts
+++ deps.ts
@@ -1,15 +1,15 @@
 import coffee from "https://deno.land/x/coffee@1.0.0/mod.ts";
-export { encode } from "https://deno.land/std@0.89.0/encoding/base64.ts";
+export { encode } from "https://deno.land/std@0.104.0/encoding/base64.ts";
 export { link, Markdown } from "https://deno.land/x/deno_markdown@0.3/mod.ts";
 
-export { green, red } from "https://deno.land/std@0.89.0/fmt/colors.ts";
+export { green, red } from "https://deno.land/std@0.104.0/fmt/colors.ts";
 export { coffee };
 
-export { parse } from "https://deno.land/std@0.89.0/flags/mod.ts";
-export type { Args } from "https://deno.land/std@0.89.0/flags/mod.ts";
+export { parse } from "https://deno.land/std@0.104.0/flags/mod.ts";
+export type { Args } from "https://deno.land/std@0.104.0/flags/mod.ts";
 
 // Test dependencies
 export {
   assert,
   assertEquals,
-} from "https://deno.land/std@0.89.0/testing/asserts.ts";
+} from "https://deno.land/std@0.104.0/testing/asserts.ts";
